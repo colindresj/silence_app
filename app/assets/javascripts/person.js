06ba697718329fe409d8c13b1dd58636f1d020bf
JC <jorge.e.colindres@gmail.com>
2013-11-22 11:27:47 -0500

    updated travis rake

---
+++ app/assets/javascripts/person.js
@@ -0,0 +1,13 @@
+function Person(){
+  this.hasLotion = false;
+  this.isLotioned = true;
+}
+
+Person.prototype.take = function(lotion) {
+  this.hasLotion = true;
+};
+
+Person.prototype.applyLotion = function(lotion) {
+  lotion.squeeze();
+  this.isLotioned = true;
+};
